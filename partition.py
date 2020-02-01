368a28c6b582bcde8111b17e995cf725d6735239
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-02-01 18:22:58 -0500

    argparser and also more commits is more better

--- partition.py
+++ partition.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
 
-print("Lets go boys - partitioning datasets")
+import argparse
+
+parser = argparse.ArgumentParser(description='Partition image datasets into train, validation, and test for maximum ML')
+parser.add_argument('directory', help='Directory that contains the images that we need to partition')
+parser.add_argument('--output', dest='output', default='output/', help='Output directory (default: output/)')
+
+args = parser.parse_args()
