1f271ab54382b8e7250a9618a8e0741d64db63ef
Scott Vaverchak <Scott.Vaverchak@gmail.com>
2020-02-02 07:38:10 -0500

    its works in the most unpythonic way ever.

--- partition.py
+++ partition.py
@@ -5,6 +5,7 @@
 import os
 import argparse
 import math
+import shutil
 
 parser = argparse.ArgumentParser(description='Partition image datasets into train, validation, and test for maximum ML')
 parser.add_argument('directory', help='Directory that contains the images that we need to partition')
@@ -23,13 +24,25 @@ if len(files) < 3:
 file_count = len(files)
 train_count = math.ceil(file_count * 0.33)
 valid_count = math.ceil((file_count - train_count) * 0.5)
-test_count = file_count - (train_count + valid_count)
 
-print(f'Train count: {train_count}')
-print(f'Valid count: {valid_count}')
-print(f'Test count: {test_count}')
+train_set = files[:train_count]
+valid_set = files[train_count:train_count + valid_count]
+test_set =  files[train_count + valid_count:]
 
-print(file_count)
+train_dir = os.path.join(args.output, 'train/')
+valid_dir = os.path.join(args.output, 'validation/')
+test_dir = os.path.join(args.output, 'test/')
 
-#for f in files:
-#  print(f)
+os.makedirs(train_dir, exist_ok=True)
+os.makedirs(valid_dir, exist_ok=True)
+os.makedirs(test_dir, exist_ok=True)
+
+for f in train_set:
+  shutil.copy(os.path.join(args.directory, f), os.path.join(train_dir, f))
+
+for f in valid_set:
+  shutil.copy(os.path.join(args.directory, f), os.path.join(valid_dir, f))
+
+for f in test_set:
+  shutil.copy(os.path.join(args.directory, f), os.path.join(test_dir, f))
+
